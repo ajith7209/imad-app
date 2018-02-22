7bc286faf46c6444ecdd09b360fec557f1104353
ajith7209 <36468938+ajith7209@users.noreply.github.com>
2018-02-22 21:19:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 var counter=0;
 app.get('/counter', function (req, res) {
   counter=counter+1;
-  res.send(counter,tostring());
+  res.send(counter.tostring());
 });
 
 app.get('/ui/madi.png', function (req, res) {
