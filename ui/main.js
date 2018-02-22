803dd6f42904be48521b113973656174afa3a5dd
ajith7209 <36468938+ajith7209@users.noreply.github.com>
2018-02-22 22:01:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,25 @@
 //console.log('Loaded!');
 
-var button = document.getElementByid('counter');
+var button = document.getElementById('counter');
 
 button.onclick = function(){
     //create a request
-    var request = new XMLHttpRequest
-}
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            // take some action
+            if(request.status===200){
+             var counter = request.responseText;
+             var span = document.getElementById('count');
+             span.innerHTML =counter.toString();
+            }
+        }
+        //not done yet
+    };
+    
+    request.open('GET','http://thiyaguaj03.imad.hasura-app.io/counter',true);
+
+    request.send(null);
+};
