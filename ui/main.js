e11433a8cffe25b5454e5ad0b0601249be0b017e
ajith7209 <36468938+ajith7209@users.noreply.github.com>
2018-02-26 19:31:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,19 @@ button.onclick = function(){
 
   
 };
+
+//submit Button
+var nameInput=document.getElementById('name');
+var str =nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick =function() {
+  //make the request to the server and send the name
+  var names=['name1','name2','name3'];
+  var list='';
+  for (var i=0;i<names.length;i++){
+      list+= '<li>'+names[i] +'</li>';
+  }
+  var ul=documet.getElementById('namelist');
+  ul.innerHTML=list;
+  
+};
